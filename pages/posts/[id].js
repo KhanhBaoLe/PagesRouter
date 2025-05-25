d4779a19b4b4d30b13e698009549ca655bdc080f
KhanhBaoLe <khanh.le230201@vnuk.edu.vn>
2025-05-25 08:49:04 +0700

    Formatting the Date

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,13 +1,13 @@
-import Head from 'next/head';
+import Date from '../../components/date';
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 export default function Post({ postData }) {
     return (
         <Layout>
-            {/* Add this <Head> tag */}
-            <Head>
-                <title>{postData.title}</title>
-            </Head>
+            {/* Keep the existing code here */}
+
+            {/* Replace {postData.date} with this */}
+            <Date dateString={postData.date} />
 
             {/* Keep the existing code here */}
         </Layout>
