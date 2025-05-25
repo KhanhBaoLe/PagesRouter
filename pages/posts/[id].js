23b34cbcbccf641c106187064768f98b83f6c17a
KhanhBaoLe <khanh.le230201@vnuk.edu.vn>
2025-05-25 08:45:03 +0700

    Chapter 34: Adding title to the Post Page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,18 @@
+import Head from 'next/head';
 import Layout from '../../components/layout';
-
 import { getAllPostIds, getPostData } from '../../lib/posts';
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            {/* Add this <Head> tag */}
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
 
+            {/* Keep the existing code here */}
+        </Layout>
+    );
+}
 export async function getStaticProps({ params }) {
     // Add the "await" keyword like this:
     const postData = await getPostData(params.id);
@@ -19,17 +30,17 @@ export async function getStaticPaths() {
         fallback: false,
     };
 }
-export default function Post({ postData }) {
-    return (
-        <Layout>
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
-            {postData.date}
-            <br />
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-        </Layout>
-    );
-}
+// export default function Post({ postData }) {
+//     return (
+//         <Layout>
+//             {postData.title}
+//             <br />
+//             {postData.id}
+//             <br />
+//             {postData.date}
+//             <br />
+//             <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+//         </Layout>
+//     );
+// }
 
